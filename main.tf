3583b1e21cba3fe5a6ad42d3340c69e3a49f9119
Goku6382798 <[gautamdixi1994@gmail.com]>
2023-09-11 15:33:08 +0530

    hola

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-resource "aws_instance" "test" {
-    ami         = "ami-053b0d53c279acc90"
+resource "aws_instance" "public_instance" {
+    ami = "ami-051f7e7f6c2f40dc1"
     instance_type = "t2.micro"
+    key_name = "Test-jenkins"
+    count = 1
+    associate_public_ip_address = true
     tags = {
-        Name = "Tf-instance"
-    }  
-}
-    
+        Name = "public_instance"
+    }
+} 
